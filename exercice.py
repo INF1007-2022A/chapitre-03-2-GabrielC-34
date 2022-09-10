4777ef834824d429d65fbee21dde8a7d8e111073
Gabriel <83326462+GabrielC-34@users.noreply.github.com>
2022-09-10 18:50:06 -0400

    Exercice complété

--- exercice.py
+++ exercice.py
@@ -3,32 +3,46 @@
 
 def dissipated_power(voltage, resistance):
 	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+	return (voltage**2)/resistance
 
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
-	v1[0] # Pour accéder au X
-	v1[1] # Pour accéder au Y
-	pass
+	if(v1[0]*v2[0] + v1[1]*v2[1]) == 0:
+		estOrthogonal = True
+	else:
+		estOrthogonal = False
+	return estOrthogonal
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	nbrValues = 0
+	addValue = 0
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		if v >= 0:
+			addValue+=v
+			nbrValues+=1
+	return addValue/nbrValues
+
+
 
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
+	twenties, tens, fives, ones = 0,0,0,0
 	while value != 0:
 		if value >= 20:
-			pass
+			value-=20
+			twenties+=1
 		elif value >= 10:
-			pass
+			value -= 10
+			tens += 1
 		elif value >= 5:
-			pass
+			value -= 5
+			fives += 1
 		elif value >= 1:
-			pass
+			value -= 1
+			ones += 1
 
-	return (twenties, tens, fives, twos, ones);
+	return (twenties, tens, fives, ones);
 
 def format_base(value, base, digit_letters):
 	# Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
@@ -36,11 +50,13 @@ def format_base(value, base, digit_letters):
 	result = ""
 	abs_value = abs(value)
 	while abs_value != 0:
-		pass
+		nbrOfFactor = int(abs_value % base) #Vérifie combien de fois la nouvelle base rentre dans le nombre
+		result += digit_letters[nbrOfFactor] #Ajoute la lettre/le chiffre correspondant au résultat
+		abs_value = abs_value//base #Réajuste la valeur du nombre
 	if value < 0:
 		# TODO: Ne pas oublier d'ajouter '-' devant pour les nombres négatifs.
-		pass
-	return result
+		result+='-'
+	return result[::-1] #Inverse la string
 
 
 if __name__ == "__main__":
